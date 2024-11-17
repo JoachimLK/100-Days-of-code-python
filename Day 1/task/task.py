2bc90d408d4a21350c65161a05a496687fffd6fd
JoachimLK <joachim.l.kolle.pers@gmail.com>
2024-11-17 18:49:28 +0100

    Finished day 2 of coding

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,3 @@
-print("My name is" + " " + "Angela")
+city = str(input("What's the name of your city: \n"))
+name = str(input("What's your name: \n"))
+print("Your band name is: " + city + " " + name)
