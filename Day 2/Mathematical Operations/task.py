02f8aba19b15bca28af8155b9cf1b3b2be8757ee
JoachimLK <joachim.l.kolle.pers@gmail.com>
2024-11-23 18:32:45 +0100

    Finished day 2 of coding, little late due to exam

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,2 @@
 print("My age: " + str(12))
+print(round(3 * 3 + 3 / 3 - 7))
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,8 @@
-len(12345)
+len("Hello")
+print(type(123))
+print(type("Hello"))
+print(type(False))
+print(type(123.43))
+
+name = input("Enter your name")
+print("Number of letters in your name: " + str(len(name)))
